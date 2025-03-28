034cd618f4ef060ffe8031e566008c1d4f397c8b
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-03-28 18:57:40 +1030

    Add WorkViewModel in Models
    Remove class of WorkViewModel from Controller, to make code more modular

---
+++ SAP-MIMOSAapp/Models/WorkOrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SAP_MIMOSAapp.Controllers;
+
+namespace SAP_MIMOSAapp.Models
+{
+    public class WorkOrderViewModel
+    {
+        public List<WorkOrderMapping> workOrders { get; set; } = new();
+        public SearchRequest searchRequest { get; set; } = new();
+        public string AIResponse { get; set; } = "";
+    }
+}
