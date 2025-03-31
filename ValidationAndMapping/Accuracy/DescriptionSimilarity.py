2f7ef2153a88862a95da4a29e4b3533775da59f6
Billiamd1012 <william.darker2003@gmail.com>
2025-03-31 20:00:39 +1030

    Implemented description similarity checking

--- ValidationAndMapping/Accuracy/DescriptionSimilarity.py
+++ ValidationAndMapping/Accuracy/DescriptionSimilarity.py
@@ -1,13 +1,28 @@
 # DescriptionSimilarity compares the descriptions associated with each mapped field and scores how similar these descriptions are.
 from sentence_transformers import SentenceTransformer
+import numpy as np
+from numpy.linalg import norm
 import Mapping
 
 class DescriptionSimilarity:
     def __init__(self):
         self.model = SentenceTransformer('all-MiniLM-L6-v2')  # Lightweight BERT model
 
-    def score(mappings: list):
-        print(mappings[0].sap_description)
-        
-        # Natural language processing to compare meaning https://hyperskill.org/learn/step/22997
-        return mappings
+    def score(self, mappings: list):
+        # Get the sap and mimosa descriptions from the mapping object
+        sap_desc = mappings[0].sap_description
+        mimosa_desc = mappings[0].mimosa_description
+
+        #encode descriptions
+        sap_vector = self.model.encode(sap_desc)
+        mimosa_vector = self.model.encode(mimosa_desc)
+
+        #measure similarity https://hyperskill.org/learn/step/22997
+
+        score = np.dot(sap_vector, mimosa_vector) / (
+            norm(sap_vector) * norm(mimosa_vector)
+        )
+
+        print(score)
+
+        return score
