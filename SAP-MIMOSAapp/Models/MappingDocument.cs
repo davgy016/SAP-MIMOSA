04c27548b08f03eae33788d093e992116559dba5
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-01 22:22:45 +1030

    Added new models

---
+++ SAP-MIMOSAapp/Models/MappingDocument.cs
@@ -0,0 +1,12 @@
+﻿namespace SAP_MIMOSAapp.Models
+{
+    public class MappingDocument
+    {
+        public string mapID { get; set; } = string.Empty;
+        public string llmType { get; set; } = string.Empty;
+        public List<MappingPair> mappings { get; set; } = new List<MappingPair>();
+        public string? Color { get; set; }
+        public string aiResponse { get; set; } = string.Empty;
+    }
+}
+
