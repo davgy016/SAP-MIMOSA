fceb5125e9e5508c5a3daa2d0839874defe67063
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-01 22:22:45 +1030

    Added new models

---
+++ SAP-MIMOSAapp/Models/MappingField.cs
@@ -0,0 +1,14 @@
+﻿namespace SAP_MIMOSAapp.Models
+{
+    public class MappingField
+    {
+        public string Platform { get; set; } = string.Empty;
+        public string entityName { get; set; } = string.Empty;
+        public string fieldName { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public string DataType { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+        public string FieldLength { get; set; } = string.Empty;
+    }
+}
+
