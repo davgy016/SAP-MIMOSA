6d6117648a18b340d15788ad85dc05973d29eb57
Billiamd1012 <william.darker2003@gmail.com>
2025-04-22 15:35:51 +0930

    Restructuring

--- ValidationAndMapping/test_Tests.py
+++ ValidationAndMapping/test_Tests.py
@@ -11,15 +11,16 @@ def sample_mapping():
         sap_fieldName="AUKUS",
         sap_fieldLength=10,
         sap_dataType="String",
-        sap_description="This sentence is kinda cool",
+        sap_description="This sentence is very cool",
         mimosa_fieldName="AUKUS_MIM",
         mimosa_fieldLength=10,
-        mimosa_dataType="String",
-        mimosa_description="This sentence is kinda cool"
+        mimosa_dataType="Array<Char>",
+        mimosa_description="This sentence is cool"
     )    
     return [mapping]
 
 def test_descriptionSimilarity(sample_mapping):
     sim = DescriptionSimilarity()
     score = sim.score(mappings=sample_mapping)
+    print(score)
     assert score == pytest.approx(0.8, abs=0.2), "Descriptions are not similar"
