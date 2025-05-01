7b117fcea26ced90164a99f656c7b56d6cf9be61
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-05-01 15:54:32 +0930

    Fix Pytest

--- WebApp/ValidationAndMapping/Accuracy/Accuracy.py
+++ WebApp/ValidationAndMapping/Accuracy/Accuracy.py
@@ -6,7 +6,7 @@ from .DataType import DataType
 from .DescriptionSimilarity import DescriptionSimilarity
 from .FieldLength import FieldLength
 
-from Models import Mapping
+from WebApp.Models import Mapping
 
 
 class Accuracy:
