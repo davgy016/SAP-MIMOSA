fa9d45b79a3557a03479c3f33c6bb5f0fd7819bd
Son Tung <binlabum123@gmail.com>
2025-05-05 01:52:46 +0930

    added association matching check and include it in the accuracy scorer

--- WebApp/ValidationAndMapping/Accuracy/Accuracy.py
+++ WebApp/ValidationAndMapping/Accuracy/Accuracy.py
@@ -1,13 +1,11 @@
 # This engine class calculates the accuracy of the mappings generated using a series of criteria to generate a score.
 
 #import criteria
-from .AssociationMatching import AssociationMatching
-from .DataType import DataType
-from .DescriptionSimilarity import DescriptionSimilarity
-from .FieldLength import FieldLength
-
-from ..Models import Mapping
-
+from WebApp.ValidationAndMapping.Accuracy.AssociationMatching  import AssociationMatching
+from WebApp.ValidationAndMapping.Accuracy.DataType              import DataType
+from WebApp.ValidationAndMapping.Accuracy.DescriptionSimilarity import DescriptionSimilarity
+from WebApp.ValidationAndMapping.Accuracy.FieldLength           import FieldLength
+from WebApp.ValidationAndMapping.Models                        import Mapping
 
 class Accuracy:
     def calculateAccuracy(self, mapping:  Mapping) -> float:
@@ -21,7 +19,14 @@ class Accuracy:
                 float: The computed score as a float.
         """
         # Run the mapping through description similarity
-        ds = DescriptionSimilarity()
-        ds_score = ds.score(mapping)
+        # 1) Type match (exact or 0)
+        dt_score    = DataType.score(mapping)
+        # 2) Textual similarity of descriptions (0–1)
+        desc_score  = DescriptionSimilarity().score(mapping)
+        # 3) Relative field‐length closeness (0–1)
+        fl_score    = FieldLength.score(mapping)
+        # 4) Entity‐level association (0–1)
+        assoc_score = AssociationMatching.score(mapping)
 
-        return ds_score
+        # Simple unweighted average:
+        return (dt_score + desc_score + fl_score + assoc_score) / 4.0
