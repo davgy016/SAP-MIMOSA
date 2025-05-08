f9935ca92b82199fea56e230453e0eb98e939c05
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-05-08 17:36:01 +0930

    Delete quality with its files,  only keep InfoOmmitted  and moved into Accuracy

--- ValidationAndMapping/Quality/Concisness.py
+++
@@ -1,5 +0,0 @@
-# Concisness is an engine class that calculates how much of the model output is useful and how much is unwanted filler.
-
-class Concisness:
-    def score(content: str): 
-        return float
